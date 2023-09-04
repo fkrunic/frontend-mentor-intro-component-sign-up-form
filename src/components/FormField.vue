725718365f806a7c108e5c48344b605b1aaec3cf
Filip Krunic <fkrunic9@gmail.com>
2023-09-04 10:28:12 -0700

    form field aesthetics

--- src/components/FormField.vue
+++ src/components/FormField.vue
@@ -3,7 +3,7 @@ defineProps<{ field: string }>()
 </script>
 
 <template>
-  <div class="w-full p-4 border-solid border-2 border-grayish-blue rounded">
+  <div class="w-full p-4 border-solid border-[1px] border-grayish-blue/50 rounded">
     <p class="text-sm font-semibold text-dark-blue/70">{{ field }}</p>
   </div>    
 </template>
