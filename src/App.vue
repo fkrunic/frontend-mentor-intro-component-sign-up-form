100f9a8d766056a142993180a4852d355d982929
Filip Krunic <fkrunic9@gmail.com>
2023-09-04 09:56:21 -0700

    assets setup

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  document.body.className = 'font-poppins bg-red/40'
+})
 </script>
 
 <template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
+    screens: {
+      desktop: '1440px'
+    },
+    fontFamily: {
+      poppins: 'Poppins, sans-serif'
+    },
+    colors: {
+      red: 'hsl(0, 100%, 74%)',
+      green: 'hsl(154, 59%, 51%)',
+      blue: 'hsl(248, 32%, 49%)',
+      'dark-blue': 'hsl(249, 10%, 26%)',
+      'grayish-blue': 'hsl(246, 25%, 77%)'
+    },
+    backgroundImage: {
+      'desktop-wallpaper': "url('./assets/bg-intro-desktop.png')",
+      'mobile-wallpaper': "url('./assets/bg-intro-mobile.png')",
+    },
     extend: {},
   },
   plugins: [],
